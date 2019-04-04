04b0bbda1ab73704f74f9309644de84ef13603a2
Razvan Laurus <razvanlaurus@gmail.com>
2019-04-04 14:27:57 +0200

    feat: avoid subscribing to health_status_changed_event

--- src/marathon-event-bus.js
+++ src/marathon-event-bus.js
@@ -3,8 +3,7 @@ import {streamEvents} from './marathon'
 import logger from './logger'
 
 let eventTypes = [
-    'status_update_event',
-    'health_status_changed_event'
+    'status_update_event'
 ]
 
 export default class MarathonEventBus extends EventEmitter {
